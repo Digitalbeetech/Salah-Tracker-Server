cb0a85236d922616519524905c1987acf320afaf
UmerKhan1998 <devUk9298@gmail.com>
2025-11-05 03:18:15 -0800

    fix: sync package-lock.json with package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { MyLoggerModule } from './my-logger/my-logger.module';
 import { ConfigModule } from '@nestjs/config';
+
 import { DatabaseModule } from './database/database.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
