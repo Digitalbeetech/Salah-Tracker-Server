6215841eb1f6053bfd5c111705049a4746fe71c7
UmerKhan1998 <devUk9298@gmail.com>
2025-11-05 04:51:04 -0800

    Fix lockfile sync issue

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['http://localhost:3000'], // your frontend URL
+
     credentials: true, // allow cookies
   });
   app.setGlobalPrefix('api');
